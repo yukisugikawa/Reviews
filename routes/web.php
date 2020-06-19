d6dae2060a82f2a7147228f5f651103471b990d3
yukisugikawa <291789sy@gmail.com>
2020-06-19 22:51:42 +0900

    ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/posts/{post}/likes', 'LikesController@store');
+Route::post('/posts/{post}/likes/{like}', 'LikesController@destroy');
