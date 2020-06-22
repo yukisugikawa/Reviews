3815ac2a72346e8d670fe08eac14d39a2bc728ae
yukisugikawa <291789sy@gmail.com>
2020-06-22 22:56:58 +0900

    ログイン、ユーザーのスカフォールド生成

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 
 Route::post('/posts/{post}/likes', 'LikesController@store');
 Route::post('/posts/{post}/likes/{like}', 'LikesController@destroy');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
